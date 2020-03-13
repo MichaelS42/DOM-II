56e6e5fc69a73871ca4feb6a276f62e13373a290
Michael Sanchez <michaelsanchez42@live.com>
2020-03-12 22:11:09 -0600

    completed one event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+
+const funBus = document.querySelector('.intro img')
+funBus.addEventListener('dblclick', () => {
+    funBus.src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/780593/cat-lambda.png"
+})
+
